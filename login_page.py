2d43c88a6b3c31f976a22d42d9c6739290030167
AleMalo <olamal31@gmail.com>
2025-02-26 16:25:31 +0100

    + substituted color names + added pictures in the 'how to play' screen

--- login_page.py
+++ login_page.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import *
 from plansza import *
 from settings import *
 
@@ -12,53 +13,78 @@ class LoginPage:
         self.root = tk.Tk()
         self.root.title('Log in')
         self.root.geometry('1440x810')
-        self.root.configure(bg=PLAYER1COLOR1)
+        self.root.configure(bg=GC1)
         self.root.resizable(False, False)
 
         # starting page
 
         self.game_title = tk.Label(self.root, text='[GAME NAME]', font=FONT1)
-        self.game_title.configure(bg=PLAYER1COLOR1, fg=PLAYER1COLOR3)
+        self.game_title.configure(bg=GC1, fg=GC3)
         self.game_title.place(relx=0.5, rely=0.3, anchor='center')
 
         self.game_button = tk.Button(self.root, text='START', font=FONT2, bd=0, command=self.start_tk_game)
-        self.game_button.configure(bg=PLAYER1COLOR2, fg=PLAYER1COLOR3, activebackground=PLAYER1COLOR3,
-                                   activeforeground=PLAYER1COLOR2)
+        self.game_button.configure(bg=GC2, fg=GC3, activebackground=GC3,
+                                   activeforeground=GC2)
         self.game_button.place(relx=0.5, rely=0.5, anchor='center', relwidth=0.333, relheight=0.1)
 
-        self.how_button = tk.Button(self.root, text='HOW TO PLAY', font=FONT2, bd=0)
-        self.how_button.configure(bg=PLAYER1COLOR2, fg=PLAYER1COLOR3, activebackground=PLAYER1COLOR3,
-                                    activeforeground=PLAYER1COLOR2)
+        self.how_button = tk.Button(self.root, text='HOW TO PLAY', font=FONT2, bd=0, command=self.open_how)
+        self.how_button.configure(bg=GC2, fg=GC3, activebackground=GC3,
+                                  activeforeground=GC2)
         self.how_button.place(relx=0.5, rely=0.625, anchor='center', relwidth=0.333, relheight=0.1)
 
-        # login page elements - hidden at the start
+        # "how to play" page
+
+        self.chest_open1_image = PhotoImage(file='co1_big.PNG')
+        self.chest_closed1_image = PhotoImage(file='cc1_big.PNG')
+        self.chest_open2_image = PhotoImage(file='co2_big.PNG')
+        self.chest_closed2_image = PhotoImage(file='cc2_big.PNG')
+        self.player1_image = PhotoImage(file='hacker1_big.PNG')
+        self.player2_image = PhotoImage(file='hacker2_big.png')
+
+        self.co1_label = tk.Label(self.root, image=self.chest_open1_image, borderwidth=0)
+        self.co1_label.place(x=200, y=-662, anchor='center')
+        self.cc1_label = tk.Label(self.root, image=self.chest_closed1_image, borderwidth=0)
+        self.cc1_label.place(x=200, y=-442, anchor='center')
+        self.p1_label = tk.Label(self.root, image=self.player1_image, borderwidth=0)
+        self.p1_label.place(x=200, y=-185, anchor='center')
+
+        self.co2_label = tk.Label(self.root, image=self.chest_open2_image, borderwidth=0)
+        self.co2_label.place(x=920, y=-676, anchor='center')
+        self.cc2_label = tk.Label(self.root, image=self.chest_closed2_image, borderwidth=0)
+        self.cc2_label.place(x=920, y=-456, anchor='center')
+        self.p2_label = tk.Label(self.root, image=self.player2_image, borderwidth=0)
+        self.p2_label.place(x=920, y=-185, anchor='center')
+
+        # player 1 elements
 
         self.player_text = tk.Label(self.root, text='PLAYER 1', font=FONT1)
-        self.player_text.configure(bg=PLAYER1COLOR1, fg=PLAYER1COLOR3)
+        self.player_text.configure(bg=P1C1, fg=P1C3)
         self.player_text.place(relx=0.5, rely=-0.25, anchor='center')
 
         self.enter_text = tk.Label(self.root, text='Enter your password:', font=FONT2)
-        self.enter_text.configure(bg=PLAYER1COLOR1, fg=PLAYER1COLOR3)
+        self.enter_text.configure(bg=P1C1, fg=P1C3)
         self.enter_text.place(relx=0.5, rely=-0.425, anchor='center')
 
         self.password_entry = tk.Entry(self.root, show='*', font=FONT2)
-        self.password_entry.configure(bg=PLAYER1COLOR2, fg=PLAYER1COLOR1)
+        self.password_entry.configure(bg=P1C2, fg=P1C1)
         self.password_entry.place(relx=0.5, rely=-0.5, anchor='center', relwidth=0.5, relheight=0.067)
 
         self.password_entry.bind('<Button-1>', self.erase_input)
 
         self.info_text = tk.Label(self.root, text=INFO, font=FONT2, justify='center')
-        self.info_text.configure(bg=PLAYER1COLOR1, fg=PLAYER1COLOR3)
+        self.info_text.configure(bg=P1C1, fg=P1C3)
         self.info_text.place(relx=0.5, rely=-0.625, anchor='center')
 
         self.change_button = tk.Button(self.root, text='CONFIRM', font=FONT2, bd=0, command=self.change_players)
-        self.change_button.configure(bg=PLAYER1COLOR2, fg=PLAYER1COLOR3, activebackground=PLAYER1COLOR3,
-                                     activeforeground=PLAYER1COLOR2)
+        self.change_button.configure(bg=P1C2, fg=P1C3, activebackground=P1C3,
+                                     activeforeground=P1C2)
         self.change_button.place(relx=0.5, rely=-0.75, anchor='center', relwidth=0.333, relheight=0.067)
 
+        # player 2 element
+
         self.start_button = tk.Button(self.root, text='CONFIRM', font=FONT2, bd=0, command=self.enter_game)
-        self.start_button.configure(bg=PLAYER1COLOR2, fg=PLAYER1COLOR1, activebackground=PLAYER1COLOR1,
-                                    activeforeground=PLAYER1COLOR2)
+        self.start_button.configure(bg=P2C2, fg=P2C3, activebackground=P2C3,
+                                    activeforeground=P2C2)
         self.start_button.place(relx=0.5, rely=-0.75, anchor='center', relwidth=0.333, relheight=0.067)
 
         self.root.mainloop()
@@ -71,11 +97,11 @@ class LoginPage:
             self.password_entry.bind('<Button-1>', self.erase_input)
             print('password is wrong')
         else:
-            self.root.configure(bg=PLAYER1COLOR3)
-            self.player_text.config(text='PLAYER 2', bg=PLAYER1COLOR3, fg=PLAYER1COLOR1)
-            self.enter_text.config(bg=PLAYER1COLOR3, fg=PLAYER1COLOR1)
-            self.password_entry.config(fg=PLAYER1COLOR1)
-            self.info_text.config(bg=PLAYER1COLOR3, fg=PLAYER1COLOR1)
+            self.root.config(bg=P2C1)
+            self.player_text.config(text='PLAYER 2', bg=P2C1, fg=P2C3)
+            self.enter_text.config(bg=P2C1, fg=P2C3)
+            self.password_entry.config(bg=P2C2, fg=P2C3)
+            self.info_text.config(bg=P2C1, fg=P2C3)
             self.change_button.place(rely=-0.75)
             self.start_button.place(rely=0.75)
             self.password_entry.delete(0, tk.END)
@@ -96,7 +122,7 @@ class LoginPage:
 
     def erase_input(self, event=None):
         if self.password_entry.cget('fg') == '#ff0000':
-            self.password_entry.config(fg=PLAYER1COLOR1)
+            self.password_entry.config(fg=P1C1)
             self.password_entry.delete(0, tk.END)
             print('erased')
 
@@ -105,6 +131,7 @@ class LoginPage:
         Plansza()
 
     def start_tk_game(self):
+        self.root.config(bg=P1C1)
         self.game_title.place(rely=-0.3)
         self.game_button.place(rely=-0.5)
         self.how_button.place(rely=-0.625)
@@ -113,5 +140,18 @@ class LoginPage:
         self.password_entry.place(rely=0.5)
         self.info_text.place(rely=0.625)
         self.change_button.place(rely=0.75)
+
+    def open_how(self):
+        self.game_title.place(rely=-0.3)
+        self.game_button.place(rely=-0.5)
+        self.how_button.place(rely=-0.625)
+
+        self.co1_label.place(y=662)
+        self.cc1_label.place(y=442)
+        self.p1_label.place(y=185)
+        self.co2_label.place(y=676)
+        self.cc2_label.place(y=456)
+        self.p2_label.place(y=185)
+
 
 LoginPage()
