46efc9be508170c3a650727e444f5ea76be8bf44
AleMalo <olamal31@gmail.com>
2025-02-27 20:56:26 +0100

    grouped elements and added the 'how to play' page

--- login_page.py
+++ login_page.py
@@ -16,46 +16,79 @@ class LoginPage:
         self.root.configure(bg=GC1)
         self.root.resizable(False, False)
 
-        # starting page
+
+        # START PAGE
+
+        # text
 
         self.game_title = tk.Label(self.root, text='[GAME NAME]', font=FONT1)
         self.game_title.configure(bg=GC1, fg=GC3)
         self.game_title.place(relx=0.5, rely=0.3, anchor='center')
 
+        # buttons
+
         self.game_button = tk.Button(self.root, text='START', font=FONT2, bd=0, command=self.start_tk_game)
         self.game_button.configure(bg=GC2, fg=GC3, activebackground=GC3,
                                    activeforeground=GC2)
-        self.game_button.place(relx=0.5, rely=0.5, anchor='center', relwidth=0.333, relheight=0.1)
+        self.game_button.place(relx=0.5, rely=0.5, anchor='center', relwidth=0.333, height=75)
 
         self.how_button = tk.Button(self.root, text='HOW TO PLAY', font=FONT2, bd=0, command=self.open_how)
         self.how_button.configure(bg=GC2, fg=GC3, activebackground=GC3,
                                   activeforeground=GC2)
-        self.how_button.place(relx=0.5, rely=0.625, anchor='center', relwidth=0.333, relheight=0.1)
+        self.how_button.place(relx=0.5, rely=0.625, anchor='center', relwidth=0.333, height=75)
 
-        # "how to play" page
+
+        # 'HOW TO PLAY' PAGE
+
+        # images
 
         self.chest_open1_image = PhotoImage(file='co1_big.PNG')
         self.chest_closed1_image = PhotoImage(file='cc1_big.PNG')
         self.chest_open2_image = PhotoImage(file='co2_big.PNG')
         self.chest_closed2_image = PhotoImage(file='cc2_big.PNG')
-        self.player1_image = PhotoImage(file='hacker1_big.PNG')
-        self.player2_image = PhotoImage(file='hacker2_big.png')
+        self.player1_image = PhotoImage(file='hacker1.PNG')
+        self.player2_image = PhotoImage(file='hacker2.png')
 
         self.co1_label = tk.Label(self.root, image=self.chest_open1_image, borderwidth=0)
-        self.co1_label.place(x=200, y=-662, anchor='center')
+        self.co1_label.place(x=1231, y=-662, anchor='center')
         self.cc1_label = tk.Label(self.root, image=self.chest_closed1_image, borderwidth=0)
-        self.cc1_label.place(x=200, y=-442, anchor='center')
+        self.cc1_label.place(x=1070, y=-442, anchor='center')
         self.p1_label = tk.Label(self.root, image=self.player1_image, borderwidth=0)
-        self.p1_label.place(x=200, y=-185, anchor='center')
+        self.p1_label.configure(bg=GC1)
+        self.p1_label.place(x=920, y=-185, anchor='center')
 
         self.co2_label = tk.Label(self.root, image=self.chest_open2_image, borderwidth=0)
-        self.co2_label.place(x=920, y=-676, anchor='center')
+        self.co2_label.place(x=1231, y=-676, anchor='center')
         self.cc2_label = tk.Label(self.root, image=self.chest_closed2_image, borderwidth=0)
-        self.cc2_label.place(x=920, y=-456, anchor='center')
+        self.cc2_label.place(x=1070, y=-456, anchor='center')
         self.p2_label = tk.Label(self.root, image=self.player2_image, borderwidth=0)
+        self.p2_label.configure(bg=GC1)
         self.p2_label.place(x=920, y=-185, anchor='center')
 
-        # player 1 elements
+        # text
+
+        self.rules_label = tk.Label(self.root, text=RULES, font=FONT2)
+        self.rules_label.configure(bg=GC1, fg=GC3)
+        self.rules_label.place(relx=0.25, y=-343.75, anchor='center')
+
+        self.p1_images_label = tk.Label(self.root, text='Player 1 Elements', font=FONT1)
+        self.p1_images_label.configure(bg=GC1, fg=GC3)
+        self.p1_images_label.place(relx=0.75, y=-56, anchor='center')
+
+        self.p2_images_label = tk.Label(self.root, text='Player 2 Elements', font=FONT1)
+        self.p2_images_label.configure(bg=GC1, fg=GC3)
+        self.p2_images_label.place(relx=0.75, y=-56, anchor='center')
+
+        # button
+
+        self.back_button = tk.Button(self.root, text='BACK', font=FONT2, borderwidth=0)
+        self.back_button.configure(bg=GC2, fg=GC3, activebackground=GC3, activeforeground=GC2)
+        self.back_button.place(relx=0.5, y=-760, relwidth=0.33, height=75, anchor='center')
+
+
+        # PASSWORD ENTERING
+
+        # text
 
         self.player_text = tk.Label(self.root, text='PLAYER 1', font=FONT1)
         self.player_text.configure(bg=P1C1, fg=P1C3)
@@ -65,22 +98,24 @@ class LoginPage:
         self.enter_text.configure(bg=P1C1, fg=P1C3)
         self.enter_text.place(relx=0.5, rely=-0.425, anchor='center')
 
+        self.info_text = tk.Label(self.root, text=INFO, font=FONT2, justify='center')
+        self.info_text.configure(bg=P1C1, fg=P1C3)
+        self.info_text.place(relx=0.5, rely=-0.625, anchor='center')
+
+        # entry
+
         self.password_entry = tk.Entry(self.root, show='*', font=FONT2)
         self.password_entry.configure(bg=P1C2, fg=P1C1)
         self.password_entry.place(relx=0.5, rely=-0.5, anchor='center', relwidth=0.5, relheight=0.067)
 
-        self.password_entry.bind('<Button-1>', self.erase_input)
+        # buttons
 
-        self.info_text = tk.Label(self.root, text=INFO, font=FONT2, justify='center')
-        self.info_text.configure(bg=P1C1, fg=P1C3)
-        self.info_text.place(relx=0.5, rely=-0.625, anchor='center')
+        self.password_entry.bind('<Button-1>', self.erase_input)
 
         self.change_button = tk.Button(self.root, text='CONFIRM', font=FONT2, bd=0, command=self.change_players)
         self.change_button.configure(bg=P1C2, fg=P1C3, activebackground=P1C3,
                                      activeforeground=P1C2)
         self.change_button.place(relx=0.5, rely=-0.75, anchor='center', relwidth=0.333, relheight=0.067)
-
-        # player 2 element
 
         self.start_button = tk.Button(self.root, text='CONFIRM', font=FONT2, bd=0, command=self.enter_game)
         self.start_button.configure(bg=P2C2, fg=P2C3, activebackground=P2C3,
@@ -146,12 +181,17 @@ class LoginPage:
         self.game_button.place(rely=-0.5)
         self.how_button.place(rely=-0.625)
 
-        self.co1_label.place(y=662)
-        self.cc1_label.place(y=442)
-        self.p1_label.place(y=185)
-        self.co2_label.place(y=676)
-        self.cc2_label.place(y=456)
-        self.p2_label.place(y=185)
+        self.rules_label.place(y=343.75)
+        self.p1_images_label.place(y=76)
+        self.p2_images_label.place(y=377)
+        self.back_button.place(y=725)
+
+        self.co1_label.place(y=205)
+        self.cc1_label.place(y=205)
+        self.p1_label.place(y=205)
+        self.co2_label.place(y=510)
+        self.cc2_label.place(y=510)
+        self.p2_label.place(y=510)
 
 
 LoginPage()
