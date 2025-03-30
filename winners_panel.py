a4ac578e82cc3f465b0aaf1b984de969f0bd2aab
amalo <olamal31@gmail.com>
2025-03-30 15:07:12 +0200

    added password checks with winners

--- winners_panel.py
+++ winners_panel.py
@@ -15,6 +15,8 @@ class WinnerPanel:
         self.correct_password2 = p2
         self.guessed_password2 = ''
 
+        self.chance = 1
+
         # PLAYER 1
 
         self.p1_label = tk.Label(self.root, text='PLAYER 1', font=FONT2)
@@ -33,10 +35,6 @@ class WinnerPanel:
         self.p1_info_label.configure(bg=GC1, fg=GC3)
         self.p1_info_label.place(relx=0.25, rely=0.5375, anchor='center')
 
-        self.p1_input_button = tk.Button(self.root, text='INPUT', font=FONT3, bd=0)
-        self.p1_input_button.configure(bg=GC2, fg=GC3, activebackground=GC3, activeforeground=GC2)
-        self.p1_input_button.place(relx=0.25, rely=0.65, relwidth=0.25, height=75, anchor='center')
-
         # PLAYER 2
 
         self.p2_label = tk.Label(self.root, text='PLAYER 2', font=FONT2)
@@ -51,20 +49,47 @@ class WinnerPanel:
         self.p2_password_entry.configure(bg=GC2, fg=GC3)
         self.p2_password_entry.place(relx=0.75, rely=0.45, relwidth=0.333, relheight=0.067, anchor='center')
 
-        self.p1_info_label = tk.Label(self.root, text='password had the clues given in the game', font=FONT4)
-        self.p1_info_label.configure(bg=GC1, fg=GC3)
-        self.p1_info_label.place(relx=0.75, rely=0.5375, anchor='center')
-
-        self.p1_input_button = tk.Button(self.root, text='INPUT', font=FONT3, bd=0)
-        self.p1_input_button.configure(bg=GC2, fg=GC3, activebackground=GC3, activeforeground=GC2)
-        self.p1_input_button.place(relx=0.75, rely=0.65, relwidth=0.25, height=75, anchor='center')
+        self.p2_info_label = tk.Label(self.root, text='password had the clues given in the game', font=FONT4)
+        self.p2_info_label.configure(bg=GC1, fg=GC3)
+        self.p2_info_label.place(relx=0.75, rely=0.5375, anchor='center')
 
         # TOGETHER
 
-        self.check_button = tk.Button(self.root, text='CHECK', font=FONT3, bd=0)
+        self.check_button = tk.Button(self.root, text='CHECK', font=FONT3, bd=0, command=self.check_passwords)
         self.check_button.configure(bg=GC2, fg=GC3, activebackground=GC3, activeforeground=GC2)
-        self.check_button.place(relx=0.5, rely=0.85, relwidth=0.667, height=75, anchor='center')
+        self.check_button.place(relx=0.5, rely=0.65, relwidth=0.667, height=75, anchor='center')
+
+        # WINNING TEXT
+
+        self.winning_player_label = tk.Label(self.root, text='', font=FONT1)
+        self.winning_player_label.configure(bg=GC1, fg=GC3)
+        self.winning_player_label.place(relx=0.5, rely=0.5, anchor='center')
 
         self.root.mainloop()
 
-WinnerPanel()
+    def check_passwords(self):
+        self.guessed_password1 = self.p1_password_entry.get()
+        self.guessed_password2 = self.p2_password_entry.get()
+        if self.chance != 3:
+            if self.guessed_password1 == self.correct_password2 or self.guessed_password2 == self.correct_password1:
+                self.p1_label.destroy()
+                self.p1_enter_label.destroy()
+                self.p1_password_entry.destroy()
+                self.p1_info_label.destroy()
+                self.p2_label.destroy()
+                self.p2_enter_label.destroy()
+                self.p2_password_entry.destroy()
+                self.p2_info_label.destroy()
+                self.check_button.destroy()
+            if self.guessed_password1 == self.correct_password2 and self.guessed_password2 == self.correct_password1:
+                self.winning_player_label.config(text='BOTH PLAYERS WON!')
+            elif self.guessed_password1 == self.correct_password2 and self.guessed_password2 != self.correct_password1:
+                self.winning_player_label.config(text='PLAYER 1 WON!')
+            elif self.guessed_password1 != self.correct_password2 and self.guessed_password2 == self.correct_password1:
+                self.winning_player_label.config(text='PLAYER 2 WON!')
+            self.chance += 1
+        else:
+            self.winning_player_label.config(text='NOONE WON!')
+
+
+WinnerPanel('qwerQWER1234!@#', 'QWERqwer!@#$123')
